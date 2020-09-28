39f80e3cd431c73b515df6cc4d5871555ae997b4
Aztec <salgorog@hotmail.com>
2020-09-27 18:12:25 -0600

    implemented .filter & .map

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const inquirer = require('inquirer');
 
 const promptUser = () => {
@@ -133,16 +136,11 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
-
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
